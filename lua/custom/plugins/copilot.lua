533a487c175500a8e93072f9d750f09f1aea6958
Daniel Munoz <danielm@remitly.com>
2025-05-27 18:49:17 -0700

    Added Copilot plugin

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<M-CR>",
+					accept_word = "<M-j>",
+					accept_line = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 20.x
+			server_opts_overrides = {},
+		})
+	end,
+}
