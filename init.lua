7d75e95ea07355fb5d7575d5366315bf5513d61d
Daniel Munoz <danielm@remitly.com>
2025-05-30 22:47:28 -0700

    Small improvements in configuration

--- init.lua
+++ init.lua
@@ -1032,11 +1032,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+-- keymaps for window resizing
+vim.keymap.set('n', '<C-S-j>', '<C-w>+', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-S-k>', '<C-w>-', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-S-h>', '<C-w><', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-S-l>', '<C-w>>', { desc = 'Increase window width' })
 
 -- keymaps for splits and tabs
 -- window management
