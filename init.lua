00620cd1893d0fc08d4a07570b9f7bacfa5a6f60
Daniel Munoz <danielm@remitly.com>
2025-06-05 10:06:20 -0700

    Addressing PR comment

--- init.lua
+++ init.lua
@@ -437,7 +437,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', { desc = '[S]earch [A]ll Files' })
+      vim.keymap.set('n', '<leader>sa', '<cmd>Telescope find_files follow=true no_ignore=true hidden=true <CR>', { desc = '[S]earch [A]ll Files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
