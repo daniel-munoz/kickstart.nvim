b640c04a79fa65feba8cd72d697f874034851a3a
Daniel Munoz <dmunozg@gmail.com>
2025-06-17 14:11:53 -0700

    Using just PROJECT_CONTEXT to enable/disable AI plugins for consistency

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -1,7 +1,7 @@
 return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    enabled = vim.fn.getenv("NVIM_COPILOT") == "true",
+    enabled = vim.fn.getenv 'PROJECT_CONTEXT' == 'personal',
     dependencies = {
       { 'zbirenbaum/copilot.lua' },
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
