d9a9f31b0fa88a17001cc05a3f7cf7851f4e24bc
rheia475 <Andrew.Sparr@cpb-software.com>
2026-01-27 16:08:09 +0100

    feat: add Telescope binding for searching through commands (#1675)

--- init.lua
+++ init.lua
@@ -436,6 +436,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sa', '<cmd>Telescope find_files follow=true no_ignore=true hidden=true <CR>', { desc = '[S]earch [A]ll Files' })
 
